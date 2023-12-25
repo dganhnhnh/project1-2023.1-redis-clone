9021fc50bae22d9b329996bcc28c7ebcb3d5b454
dganhnhnh <duonganh2973@gmail.com>
2023-12-25 22:56:23 +0700

    code cmt for server process

--- server.cpp
+++ server.cpp
@@ -24,9 +24,10 @@ const size_t k_max_msg = 4096;
 
 static int32_t read_full(int fd, char *buf, size_t n) {
     while (n > 0) {
+        //-- đọc n bytes từ fd vào buf
         ssize_t rv = read(fd, buf, n);
         if (rv <= 0) {
-            return -1;  // error, or unexpected EOF
+            return -1;  //-- error, or unexpected EOF
         }
         assert((size_t)rv <= n);
         n -= (size_t)rv;
@@ -39,7 +40,7 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
     while (n > 0) {
         ssize_t rv = write(fd, buf, n);
         if (rv <= 0) {
-            return -1;  // error
+            return -1;  //-- error
         }
         assert((size_t)rv <= n);
         n -= (size_t)rv;
@@ -49,7 +50,7 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
 }
 
 static int32_t one_request(int connfd) {
-    // 4 bytes header
+    //-- 4 bytes header
     char rbuf[4 + k_max_msg + 1];
     errno = 0;
     int32_t err = read_full(connfd, rbuf, 4);
@@ -63,24 +64,23 @@ static int32_t one_request(int connfd) {
     }
 
     uint32_t len = 0;
-    memcpy(&len, rbuf, 4);  // assume little endian
+    memcpy(&len, rbuf, 4);  //-- assume little endian
     if (len > k_max_msg) {
         msg("too long");
         return -1;
     }
 
-    // request body
+    //-- request body
     err = read_full(connfd, &rbuf[4], len);
     if (err) {
         msg("read() error");
         return err;
     }
 
-    // do something
     rbuf[4 + len] = '\0';
     printf("client says: %s\n", &rbuf[4]);
 
-    // reply using the same protocol
+    //-- reply using the same protocol
     const char reply[] = "world";
     char wbuf[4 + sizeof(reply)];
     len = (uint32_t)strlen(reply);
@@ -90,42 +90,47 @@ static int32_t one_request(int connfd) {
 }
 
 int main() {
+    //-- 1. SOCKET
+    //-- type: sock_stream for TCP byte stream
+    //-- domain: AF_INET
+    //-- protocol: 0 - auto
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
         die("socket()");
-    }
+    } 
 
-    // this is needed for most server applications
+    //-- this is needed for most server applications
     int val = 1;
+    //-- SO_REUSEADDR: enable server to bind to the same address if restarted
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
-    // bind
+    //-- 2. BIND
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
+    addr.sin_addr.s_addr = ntohl(0);    //-- wildcard address 0.0.0.0
     int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
     if (rv) {
         die("bind()");
     }
 
-    // listen
+    //-- 3. LISTEN
     rv = listen(fd, SOMAXCONN);
     if (rv) {
         die("listen()");
     }
 
+    //-- 4. ACCEPT
     while (true) {
-        // accept
         struct sockaddr_in client_addr = {};
         socklen_t socklen = sizeof(client_addr);
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
         if (connfd < 0) {
-            continue;   // error
+            continue;   //-- error
         }
 
         while (true) {
-            // here the server only serves one client connection at once
+            //-- here the server only serves one client connection at once
             int32_t err = one_request(connfd);
             if (err) {
                 break;
